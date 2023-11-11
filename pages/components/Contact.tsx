0a0834a5507dac7f721987301765de2cacaead67
Rishi Viswanathan <128200692+risv1@users.noreply.github.com>
2023-11-11 10:39:50 +0530

    Update Contact.tsx

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -3,17 +3,19 @@ import React, { FC, FormEvent, useRef } from "react";
 const Contact: FC = () => {
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current && emailRef.current) {
+    if (nameRef.current && emailRef.current && messageRef.current) {
       nameRef.current.value = "";
       emailRef.current.value = "";
+      messageRef.current.value = "";
     }
   };
 
   return (
-    <div className="bg-sky-800 w-1/3 h-3/5 p-4 rounded-xl">
+    <div className="bg-sky-800 h-fit p-4 rounded-xl">
       <div className="flex items-center justify-center flex-col">
         <h2 className="text-white text-2xl mb-4">Contact</h2>
         <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
@@ -29,6 +31,12 @@ const Contact: FC = () => {
             placeholder="Email"
             className="w-full p-3 border border-white rounded-xl"
           />
+          <textarea
+            ref={messageRef}
+            placeholder="Your Message"
+            className="w-full p-3 border border-white rounded-xl"
+            rows={4} 
+          ></textarea>
           <button
             type="submit"
             className="bg-white text-sky-800 text-lg px-6 py-3 rounded-xl hover:bg-yellow-300 hover:text-sky-800 transition duration-300"
